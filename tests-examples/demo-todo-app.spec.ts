a52e229150612107c4f7ccad8f7e63dbd1ebccdc
deanmrudge-ai <dean.m.rudge@gmail.com>
2026-07-24 16:17:43 +0100

    Tweaks to tests for Chapter 2 learning - including Dev tags

--- tests-examples/demo-todo-app.spec.ts
+++ tests-examples/demo-todo-app.spec.ts
@@ -146,7 +146,7 @@ test.describe('Item', () => {
     await expect(secondTodo).toHaveClass('completed');
   });
 
-  test('should allow me to un-mark items as complete', async ({ page }) => {
+  test('@dev should allow me to un-mark items as complete', async ({ page }) => {
     // create a new todo locator
     const newTodo = page.getByPlaceholder('What needs to be done?');
 
